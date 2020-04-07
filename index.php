cc362adf83c53cc4ba4cb6e2acbf188d2bdcb0ff
hidehiro <k5a9ev8r@yahoo.co.jp>
2020-04-07 09:57:21 +0900

    center producted

--- index.php
+++ index.php
@@ -40,5 +40,5 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
   <script src="./js/main.js"></script>
 </body>
-producted by<p style="text-align:center;"><a href="https://hidehiroblog.com">ヒデログ</a></p
+<p style="text-align:center;">producted by <a href="https://hidehiroblog.com">ヒデログ</a></p
 </html>
